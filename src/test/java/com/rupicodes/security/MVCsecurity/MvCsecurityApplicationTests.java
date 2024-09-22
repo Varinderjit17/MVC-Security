fe94f7e5704898fbd3ff3249c850b192ebcdc990
rupi <varinderjit17@gmail.com>
2024-09-23 10:15:05 +1200

    Initial commit

---
+++ src/test/java/com/rupicodes/security/MVCsecurity/MvCsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rupicodes.security.MVCsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvCsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
